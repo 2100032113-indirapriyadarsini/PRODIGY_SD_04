d62aea0e020dd10a60cfeb18127f6f18fe9aa0b8
2100032113-indirapriyadarsini <110585502+2100032113-indirapriyadarsini@users.noreply.github.com>
2024-06-27 12:44:50 +0530

    Add files via upload

---
+++ prodigy_sd_04.py
@@ -0,0 +1,53 @@
+def print_sudoku(board):
+    for row in board:
+        print(" ".join(str(num) if num != 0 else '.' for num in row))
+
+def is_valid(board, row, col, num):
+    # Check if the number is not in the current row, column, or 3x3 sub-grid
+    for x in range(9):
+        if board[row][x] == num or board[x][col] == num:
+            return False
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+def main():
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    print("Unsolved Sudoku puzzle:")
+    print_sudoku(board)
+    
+    if solve_sudoku(board):
+        print("\nSolved Sudoku puzzle:")
+        print_sudoku(board)
+    else:
+        print("No solution exists")
+
+if __name__ == "__main__":
+    main()
